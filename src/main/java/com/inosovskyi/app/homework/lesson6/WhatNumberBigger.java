0d82761f969d8a08c072be7c46c573ff3c3cd2e2
Goralive <nosovskyigor@gmail.com>
2018-04-05 21:47:14 +0300

    Adding one hometask array and work

--- src/main/java/com/inosovskyi/app/homework/lesson6/WhatNumberBigger.java
+++ src/main/java/com/inosovskyi/app/homework/lesson6/WhatNumberBigger.java
@@ -1,4 +1,26 @@
 package com.inosovskyi.app.homework.lesson6;
 
+import java.util.Scanner;
+
 public class WhatNumberBigger {
+
+    public static void evenNumber (){
+
+        System.out.println("Greetings, write two numbers, and you will see what number is bigger\nWrite first integer: ");
+        Scanner sc = new Scanner(System.in);
+
+        int numberOne = Integer.parseInt(sc.nextLine());
+        System.out.println("Write the second integer: ");
+        int numberTwo = Integer.parseInt(sc.nextLine());
+
+        if (numberTwo > numberOne && numberTwo != numberOne){
+            System.out.println("Number two is bigger");
+        }
+        if (numberOne > numberTwo && numberOne != numberTwo){
+            System.out.println("Number one is bigger");
+        }
+        if (numberOne == numberTwo) {
+            System.out.println("Numbers seems to be equal");
+        }
+    }
 }
---
+++ src/main/java/com/inosovskyi/runners/lesson11/RunnerTransitionString.java
@@ -0,0 +1,16 @@
+package com.inosovskyi.runners.lesson11;
+
+import com.inosovskyi.app.classwork.lesson11.Palindrome;
+import com.inosovskyi.app.classwork.lesson11.TransitionString;
+
+public class RunnerTransitionString {
+    public static void main(String[] args) {
+
+
+//        TransitionString transitionString = new TransitionString();
+//        transitionString.stringHistory();
+        Palindrome palindrome = new Palindrome();
+        palindrome.checkPalindrome();
+
+    }
+}
